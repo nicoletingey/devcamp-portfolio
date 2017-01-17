24c2e32cd772029174527e3cf53d6e333ae2df30
Nicole Tingey <nicole.tingey1@gmail.com>
2017-01-17 11:56:36 -0700

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class:"source-greeting")
     end
   end
+
+  def copyright_generator
+     TingViewTool::Renderer.copyright 'Nicole Tingey', 'All rights reserved'
+  end
 end
