558cc6666830647fd8fddcc4770a86fea85cc606
Nicole Tingey <nicole.tingey1@gmail.com>
2017-02-07 10:49:40 -0700

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://nicole-portfolio.herokuapp.com', 'http://nicole-portfolio.herokuapp.com']
+  config.action_cable.url = 'wss://nicole-portfolio.herokuapp.com/cable'
 end
